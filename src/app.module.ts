7480ca0ddfc3609d2785e35c74db562dd9c47ebd
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-06-13 17:41:37 +0700

    refactor: remove unused AppController/AppService, add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './infra/prisma/prisma.module';
+import { ConfigModule } from './infra/config/config.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, ConfigModule],
 })
 export class AppModule {}
