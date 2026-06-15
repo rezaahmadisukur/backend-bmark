83a3e212b2ad0a29ff01341431153e06b380b5a9
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-06-15 19:30:19 +0700

    feat(app): register BookmarksModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './infra/prisma/prisma.module';
 import { ConfigModule } from './infra/config/config.module';
+import { BookmarksModule } from './modules/bookmarks/bookmarks.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule],
+  imports: [ConfigModule, PrismaModule, BookmarksModule],
 })
 export class AppModule {}
