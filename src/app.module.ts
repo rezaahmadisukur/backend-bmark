36d0f3336ed9208ecb2ae6b3d3bfd96970dbcb18
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-06-21 05:36:38 +0700

    feat(app): add CollectionsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './infra/prisma/prisma.module';
 import { ConfigModule } from './infra/config/config.module';
 import { BookmarksModule } from './modules/bookmarks/bookmarks.module';
+import { CollectionsModule } from './modules/collections/collections.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule, BookmarksModule],
+  imports: [ConfigModule, PrismaModule, BookmarksModule, CollectionsModule],
 })
 export class AppModule {}
