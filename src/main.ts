124155c7ea325433710e614e7ab9cce26b19b09e
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-06-21 05:37:39 +0700

    feat(main): set up Swagger documentation for Bookmark API

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,25 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookmark API')
+    .setDescription('API for Bookmark Management')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
