26d24e63361d7f3958f463490fe8c95e36406cc1
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-06-24 13:07:14 +0700

    feat(modules): import TagsModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { PrismaModule } from './infra/prisma/prisma.module';
 import { ConfigModule } from './infra/config/config.module';
 import { BookmarksModule } from './modules/bookmarks/bookmarks.module';
 import { CollectionsModule } from './modules/collections/collections.module';
+import { TagsModule } from './modules/tags/tags.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule, BookmarksModule, CollectionsModule],
+  imports: [
+    ConfigModule,
+    PrismaModule,
+    BookmarksModule,
+    CollectionsModule,
+    TagsModule,
+  ],
 })
 export class AppModule {}
