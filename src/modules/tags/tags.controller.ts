2b0f1d20906062da3d83f434c2e84e5b274eab50
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-06-30 14:30:10 +0700

    feat(tags): add authentication and authorization guards to TagsController

--- src/modules/tags/tags.controller.ts
+++ src/modules/tags/tags.controller.ts
@@ -6,12 +6,18 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { TagsService } from './tags.service';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
+@ApiTags('Tags')
 @Controller('tags')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
