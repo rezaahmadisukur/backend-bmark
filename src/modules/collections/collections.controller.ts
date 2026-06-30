bd1d07ee58f37ab16b8cae11d5293e5c82e50fe3
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-06-30 10:28:52 +0700

    feat(collections): add user context and authorization to collections operations

--- src/modules/collections/collections.controller.ts
+++ src/modules/collections/collections.controller.ts
@@ -6,37 +6,52 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { CollectionsService } from './collections.service';
 import { CreateCollectionDto } from './dto/create-collection.dto';
 import { UpdateCollectionDto } from './dto/update-collection.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { type JwtUser } from '../auth/strategies/jwt.strategy';
 
+@ApiTags('Collections')
 @Controller('collections')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
 export class CollectionsController {
   constructor(private readonly collectionsService: CollectionsService) {}
 
   @Get()
-  findAll() {
-    return this.collectionsService.findAll();
+  findAll(@CurrentUser() user: JwtUser) {
+    return this.collectionsService.findAll(user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.collectionsService.findOne(id);
+  findOne(@Param('id') id: string, @CurrentUser() user: JwtUser) {
+    return this.collectionsService.findOne(id, user.id);
   }
 
   @Post()
-  create(@Body() createCollectionDto: CreateCollectionDto) {
-    return this.collectionsService.create(createCollectionDto);
+  create(
+    @Body() createCollectionDto: CreateCollectionDto,
+    @CurrentUser() user: JwtUser,
+  ) {
+    return this.collectionsService.create(createCollectionDto, user.id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, updateCollectionDto: UpdateCollectionDto) {
-    return this.collectionsService.update(id, updateCollectionDto);
+  update(
+    @Param('id') id: string,
+    updateCollectionDto: UpdateCollectionDto,
+    @CurrentUser() user: JwtUser,
+  ) {
+    return this.collectionsService.update(id, updateCollectionDto, user.id);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return this.collectionsService.delete(id);
+  delete(@Param('id') id: string, @CurrentUser() user: JwtUser) {
+    return this.collectionsService.delete(id, user.id);
   }
 }
