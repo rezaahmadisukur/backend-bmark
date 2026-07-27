efcece67b0f06d94942523ed5ee190a58d4119c8
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-07-27 19:32:21 +0700

    feat: enable CORS for localhost:3000 origin

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = new DocumentBuilder()
